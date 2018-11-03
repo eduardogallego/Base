c21b2d45551b9a0026cb5eea63882e2b2d142451
Eduardo Gallego <eduardo.gallego@bluu.es>
2018-11-03 13:52:04 +0100

    no message

---
+++ src/main/java/edu/spring/base/BaseApplication.java
@@ -0,0 +1,12 @@
+package edu.spring.base;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaseApplication.class, args);
+    }
+}
